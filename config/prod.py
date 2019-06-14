bfbf5250d5336587648c4b58cd83bab324f758b5
vannesschancc <vannesschancc@live.com>
2019-06-14 11:16:44 -0700

    fix prod file

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
-DEBUG = True
+import os
+
+DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:vannessc199365@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
